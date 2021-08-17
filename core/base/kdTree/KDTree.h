f530045d4a4ab87db03765d322a7c814d0e8b207
Pierre Guillou <pierre.guillou@lip6.fr>
2021-08-17 21:28:28 +0200

    [KDTree] Clean module

--- core/base/kdTree/KDTree.h
+++ core/base/kdTree/KDTree.h
@@ -8,9 +8,6 @@
 
 #pragma once
 
-#ifndef _KDTREE_H
-#define _KDTREE_H
-
 // base code includes
 #include <Debug.h>
 #include <Geometry.h> // for pow
@@ -111,11 +108,6 @@ namespace ttk {
 
     bool isLeaf();
     bool isRoot();
-
-    template <typename type>
-    inline static type abs(const type var) {
-      return (var > 0) ? var : -var;
-    }
   };
 
   template <typename dataType>
@@ -381,14 +373,13 @@ namespace ttk {
           k, coordinates, neighbours, costs, weight_index);
       }
     }
-    return;
   }
 
   template <typename dataType>
   dataType KDTree<dataType>::cost(const std::vector<dataType> &coordinates) {
     dataType cost = 0;
     for(size_t i = 0; i < coordinates.size(); i++) {
-      cost += Geometry::pow(abs(coordinates[i] - coordinates_[i]), p_);
+      cost += Geometry::pow(std::abs(coordinates[i] - coordinates_[i]), p_);
     }
     return cost;
   }
@@ -420,6 +411,5 @@ namespace ttk {
     return parent_ == nullptr;
   }
 } // namespace ttk
-#include <buildWeights.h>
 
-#endif
+#include <buildWeights.h>
