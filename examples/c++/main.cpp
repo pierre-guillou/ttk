52ef6b47b558189f7b4c1812253bc97137e8dbbf
Pierre Guillou <pierre.guillou@lip6.fr>
2021-09-20 10:56:12 +0200

    Revert "[examples] Use the new aggregated base layer header"
    This reverts commit 5f7d8a9e3869ac178e27d427186876d90e2076fd.

--- examples/c++/main.cpp
+++ examples/c++/main.cpp
@@ -19,7 +19,11 @@
 /// (C++-only) TTK component.
 
 // include the local headers
-#include <TopologyToolKit.h>
+#include <CommandLineParser.h>
+#include <MorseSmaleComplex.h>
+#include <PersistenceCurve.h>
+#include <PersistenceDiagram.h>
+#include <TopologicalSimplification.h>
 
 #include <cassert>
 #include <iostream>
