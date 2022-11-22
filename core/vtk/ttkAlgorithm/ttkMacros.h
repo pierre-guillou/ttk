6d8071aa12ead9ea5ef0e823010531f361b2a476
Pierre Guillou <pierre.guillou@lip6.fr>
2022-11-22 22:23:55 +0100

    wip

--- core/vtk/ttkAlgorithm/ttkMacros.h
+++ core/vtk/ttkAlgorithm/ttkMacros.h
@@ -93,6 +93,8 @@ using ttkSimplexIdTypeArray = vtkIntArray;
     ttkVtkTemplateMacroCase(dataType,                                     \
                             ttk::Triangulation::Type::HYBRID_IMPLICIT,    \
                             ttk::ImplicitWithPreconditions, call);        \
+    ttkVtkTemplateMacroCase(dataType, ttk::Triangulation::Type::IMPLICIT, \
+                            ttk::ImplicitNoPreconditions, call);          \
     ttkVtkTemplateMacroCase(dataType,                                     \
                             ttk::Triangulation::Type::HYBRID_PERIODIC,    \
                             ttk::PeriodicWithPreconditions, call);        \
