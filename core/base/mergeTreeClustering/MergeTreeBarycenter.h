2e4bc5a0d468bd360c333d7b25f0592efd19aa4f
Pierre Guillou <pierre.guillou@lip6.fr>
2022-12-17 22:08:18 +0100

    [base] Replace deprecated omp_set_nested

--- core/base/mergeTreeClustering/MergeTreeBarycenter.h
+++ core/base/mergeTreeClustering/MergeTreeBarycenter.h
@@ -58,7 +58,7 @@ namespace ttk {
         "MergeTreeBarycenter"); // inherited from Debug: prefix will be printed
                                 // at the beginning of every msg
 #ifdef TTK_ENABLE_OPENMP
-      omp_set_nested(1);
+      omp_set_max_active_levels(2);
 #endif
     }
     ~MergeTreeBarycenter() override = default;
